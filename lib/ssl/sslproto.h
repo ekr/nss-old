d0e4aab5a470c34fb6540cd09fd77400db4ce0fc
EKR <ekr@rtfm.com>
2014-08-08 15:36:45 -0700

    Negotiate TLS 1.3 and add an extension to signal the draft version.

--- lib/ssl/sslproto.h
+++ lib/ssl/sslproto.h
@@ -16,9 +16,16 @@
 #define SSL_LIBRARY_VERSION_TLS_1_0             0x0301
 #define SSL_LIBRARY_VERSION_TLS_1_1             0x0302
 #define SSL_LIBRARY_VERSION_TLS_1_2             0x0303
+#define SSL_LIBRARY_VERSION_TLS_1_3             0x0304
+
+/* The TLS 1.3 draft version. Used to avoid negotiating
+   between incompatible pre-standard TLS 1.3 drafts */
+#define SSL_LIBRARY_VERSION_TLS_1_3_DRAFT_VERSION  3
+
 /* Note: this is the internal format, not the wire format */
 #define SSL_LIBRARY_VERSION_DTLS_1_0            0x0302
 #define SSL_LIBRARY_VERSION_DTLS_1_2            0x0303
+#define SSL_LIBRARY_VERSION_DTLS_1_3            0x0304
 
 /* deprecated old name */
 #define SSL_LIBRARY_VERSION_3_1_TLS SSL_LIBRARY_VERSION_TLS_1_0
@@ -26,6 +33,7 @@
 /* The DTLS versions used in the spec */
 #define SSL_LIBRARY_VERSION_DTLS_1_0_WIRE       ((~0x0100) & 0xffff)
 #define SSL_LIBRARY_VERSION_DTLS_1_2_WIRE       ((~0x0102) & 0xffff)
+#define SSL_LIBRARY_VERSION_DTLS_1_3_WIRE       ((~0x0103) & 0xffff)
 
 /* Header lengths of some of the messages */
 #define SSL_HL_ERROR_HBYTES                     3
