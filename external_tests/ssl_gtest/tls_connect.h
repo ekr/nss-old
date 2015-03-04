f15cccc56c27422a8e6957908c2695c3fbfbd778
EKR <ekr@rtfm.com>
2015-03-03 17:22:05 -0800

    MT's patch version #5

---
+++ external_tests/ssl_gtest/tls_connect.h
@@ -0,0 +1,69 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef tls_connect_h_
+#define tls_connect_h_
+
+#include "sslt.h"
+
+#include "tls_agent.h"
+
+#define GTEST_HAS_RTTI 0
+#include "gtest/gtest.h"
+
+namespace nss_test {
+
+class TlsConnectTestBase : public ::testing::Test {
+ public:
+  TlsConnectTestBase(Mode mode);
+  virtual ~TlsConnectTestBase();
+
+  void SetUp();
+  void TearDown();
+  void Init();
+  void Reset();
+
+  void EnsureTlsSetup();
+  void Handshake();
+  void Connect();
+  void ConnectExpectFail();
+
+  void EnableSomeECDHECiphers();
+  void ConfigureSessionCache(SessionResumptionMode client,
+                            SessionResumptionMode server);
+  void CheckResumption(SessionResumptionMode expected);
+  void EnableAlpn();
+  void EnableSrtp();
+  void CheckSrtp();
+
+ protected:
+  Mode mode_;
+  TlsAgent* client_;
+  TlsAgent* server_;
+  std::vector<std::vector<uint8_t>> session_ids_;
+};
+
+class TlsConnectTest : public TlsConnectTestBase {
+ public:
+  TlsConnectTest() : TlsConnectTestBase(STREAM) {}
+};
+
+class DtlsConnectTest : public TlsConnectTestBase {
+ public:
+  DtlsConnectTest() : TlsConnectTestBase(DGRAM) {}
+};
+
+// A generic test class that can be either STREAM or DGRAM.  This is configured
+// in ssl_loopback_unittest.cc.  All uses of this should use TEST_P().
+class TlsConnectGeneric : public TlsConnectTestBase,
+                          public ::testing::WithParamInterface<std::string> {
+ public:
+  TlsConnectGeneric();
+};
+
+} // namespace nss_test
+
+#endif
